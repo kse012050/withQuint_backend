f5b0cf78079be484a158614f6aabb66b8abc7f76
김성은 <kse012050@naver.com>
2024-12-02 10:33:25 +0900

    회원가입 api 진행 중

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mysql = require('mysql2');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const app = express();
 
 dotenv.config();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'withquint'
+})
 
 app.set('port', process.env.PORT || 8001);
 
@@ -12,9 +21,83 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/user', (req, res, next)=>{
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+
+db.connect();
+
+app.get('/', (req, res, next)=>{
     console.log(req);
     console.log(res);
+    res.end('test')
+})
+
+
+app.get('/user', (req, res, next)=>{
+    console.log(2);
+    
+    // console.log(req.body);
+    // req.body['userId'] = 'test3';
+    // req.body['password'] = 'test';
+    // req.body['nickname'] = 'test';
+    // req.body['mobile'] = 'test';
+    // req.body['termsOfService'] = 'y';
+    // req.body['privacyPolicy'] = 'y';
+    try{
+        db.query(`DESCRIBE users`,(err, result)=>{
+            const required = result.filter(data=> data.Key !== 'PRI' && data.Type !== 'datetime' && data.Null === 'NO').map(data=> data.Field)
+            
+            const noRequired = required.filter((key)=>!req.body[key])
+            
+            if(noRequired.length){
+                res.status(400).json({result: false, error: `${noRequired.join(', ')} 값이 없습니다.`})
+                return;
+            }
+
+            const values = Object.values(req.body).map((value)=>value)
+            
+            db.query(
+                `
+                    INSERT INTO users 
+                    (${required.join(',')}, created)
+                    VALUES (${required.map(()=>'?').join(',')}, NOW())
+                `,
+                values
+                ,
+                (error, result)=>{
+                    if(error){
+                        res.status(400).json({result: false, error: '서버 에러입니다.'})
+                        throw error;
+                    };
+                    res.status(200).json({result: true}).end()
+                }
+            )
+        })
+    }catch(err){
+        console.error(err);
+    }
+})
+
+app.post('/user/check', (req, res, next)=>{
+    console.log(req.body);
+    
+    if(!req.body.type || !req.body.value) res.status(400).send(`${req.body.type} 값이 없습니다.`);
+    db.query(
+        `
+            SELECT ${req.body.type}
+            FROM users
+            WHERE ${req.body.type} = ?;
+        `,
+        // req.body.userId,
+        req.body.value,
+        (error, result)=>{
+            // console.log(result);
+            res.status(200).json({result: !result.length, message: `사용할 수 ${result.length ? '없는' : '있는'} 아이디 입니다` });
+        }
+    )
+    
 })
 
 app.use((req, res, next)=>{
