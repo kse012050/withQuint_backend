63a12032a0c6996144ae8b807323f628c1c32397
김성은 <kse012050@naver.com>
2025-05-21 22:22:56 +0900

    비밀번호 찾기, 비밀번호 변경 완료

--- controllers/find.js
+++ controllers/find.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { tryCatch, dbQuery } = require('../utils');
 
 exports.isId = tryCatch(async(req, res, next) => {
@@ -25,6 +26,10 @@ exports.isMobile = tryCatch(async(req, res, next) => {
     const { DBName, body } = req;
     const { mobile } = body;
 
+    if(!mobile) {
+        return res.status(200).json({result: true, state: false, message: `mobile 값이 없습니다.`});
+    }
+
     const [{ isMobile }] = await dbQuery(
         `
             SELECT IF(
@@ -40,14 +45,9 @@ exports.isMobile = tryCatch(async(req, res, next) => {
     next();
 })
 
-
 exports.findId = tryCatch(async(req, res, next) => {
     const { DBName, body } = req;
     const { mobile } = body;
-
-    if(!mobile) {
-        return res.status(200).json({result: true, state: false, message: `mobile 값이 없습니다.`});
-    }
 
     const [data] = await dbQuery(
         `
@@ -62,6 +62,49 @@ exports.findId = tryCatch(async(req, res, next) => {
         data.userId = data.userId.slice(0, data.userId.length - 2) + '**';
     }
 
-    return res.status(200).json({result: true, state: !!data, data});
+    return res.status(200).json({result: true, state: !!data, data, message: data ? `본인 확인이 완료되었습니다.` : `가입된 정보가 없습니다.`});
+})
+
+
+exports.findPW = tryCatch(async(req, res, next) => {
+    const { DBName, body } = req;
+    const { userId, mobile } = body;
+
+    const [{ isExist }] = await dbQuery(
+        `
+            SELECT EXISTS (
+                SELECT 1
+                FROM ${DBName}
+                WHERE mobile = ? AND userId = ?
+            ) AS isExist
+        `,
+        [mobile, userId]
+    )
+    
+    return res.status(200).json({result: true, state: !!isExist, data: {userId}, message: isExist ? `본인 확인이 완료되었습니다.` : `입력한 정보를 확인해주세요.`});
+})
+
+exports.pwChange = tryCatch(async(req, res, next) => {
+    const { DBName, body } = req;
+    let { userId, password } = body;
+
+    password = await bcrypt.hash(password, 12)
+    
+    const data = await dbQuery(
+        `
+            UPDATE ${DBName}
+            SET password = ?
+            WHERE userId = ?;
+        `,
+        [password, userId]
+    )
+
+    let message = '비밀번호가 변경되었습니다.'
+    if (data.affectedRows === 0) {
+        message = '아이디가 없습니다.'
+    } else if (data.changedRows === 0) {
+        message = '이전 비밀번호와 동일합니다.'
+    }
+    return res.status(200).json({result: true, state: data.affectedRows > 0 && data.changedRows > 0, message });
     
 })
--- routes/find.js
+++ routes/find.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { isId, isMobile, findId } = require('../controllers/find');
+const { isId, isMobile, findId, findPW, pwChange } = require('../controllers/find');
 
 const router = express.Router();
 
 router.post('/id', isMobile, findId)
+router.post('/pw', isMobile, findPW)
+router.post('/pwChange', pwChange)
 
 module.exports = router;
